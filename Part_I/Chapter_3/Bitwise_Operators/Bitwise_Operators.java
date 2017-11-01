207ef15c0a232faf22a42e44b20653f61db0ded0
James Collerton <jamescollerton@live.co.uk>
2017-11-01 08:06:16 +0000

    Done a bit more bitwising

--- Part_I/Chapter_3/Bitwise_Operators/Bitwise_Operators.java
+++ Part_I/Chapter_3/Bitwise_Operators/Bitwise_Operators.java
@@ -9,13 +9,28 @@ public class Bitwise_Operators {
 	public static void byteExamples(){
 
 		byte exampleOne = 1;
-		System.out.println("00000001: " + exampleOne);
+		System.out.println("000I00001: " + exampleOne);
 
 		byte exampleTwo = 2;
 		System.out.println("00000010: " + exampleTwo);
 
+		// The maximum values a byte can have are -128 and
+		// 127, although -128 is the same as 128. Once you
+		// get to 127 (01111111) then you wrap round to the
+		// negative numbers. 
+		byte exampleThree = -128;
+		System.out.println("10000000: " + exampleThree);
+
 		System.out.println("00000000: " + (exampleOne & exampleTwo));
 		System.out.println("00000011: " + (exampleOne | exampleTwo)); 
+		System.out.println("00000011: " + (exampleOne ^ exampleTwo));
+		System.out.println("00000000: " + (exampleOne >> 1 ));
+		System.out.println("00000001: " + (exampleTwo >> 1 ));
+
+		// So this is the same as -128 + 64 = -64
+		System.out.println("11000000: " + (exampleThree >> 1 ));
+
+		System.out.println("00000100: " + (exampleTwo << 1 ));
 
 	}
 
