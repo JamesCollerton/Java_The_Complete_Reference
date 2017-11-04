410342b4e4890f9d447fcd649e23f40190a426e5
James Collerton <jamescollerton@live.co.uk>
2017-11-04 01:39:35 +0000

    Got renaming script working

--- Part_I/Chapter_4_Operators/Bitwise_Operators/BitwiseOperators.java
+++ Part_I/Chapter_4_Operators/Bitwise_Operators/BitwiseOperators.java
@@ -1,4 +1,4 @@
-public class Bitwise_Operators {
+public class BitwiseOperators {
 	
 	public static void main(String[] args){
 		
--- Part_I/renamingScript.sh
+++ Part_I/renamingScript.sh
@@ -1,8 +1,16 @@
+currentDir=$1;
+topDir=$(PWD);
+
+cd $currentDir;
+
+echo ""
+echo "$currentDir";
+echo "$topDir";
+
 rm *.class
 
 for file in *.java;
 do
-	echo ""
 
 	echo "Changing $file file";
 	
@@ -20,3 +28,5 @@ do
 	# Change contents		
 	sed -i ''  s/$fileNoJava/$fileNoUnderscore/g $file
 done;
+
+cd $topDir
