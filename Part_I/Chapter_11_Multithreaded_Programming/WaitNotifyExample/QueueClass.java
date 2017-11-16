a89bfc1fa5a62981a8d10c4124057dd861f3777b
James Collerton <jamescollerton@live.co.uk>
2017-11-16 08:08:41 +0000

    Added synchronized

--- Part_I/Chapter_11_Multithreaded_Programming/WaitNotifyExample/QueueClass.java
+++ Part_I/Chapter_11_Multithreaded_Programming/WaitNotifyExample/QueueClass.java
@@ -1,13 +1,31 @@
 public class QueueClass {
 
 	private int n;
+	private boolean valueExists = false;
 
 	public synchronized void put(int n){
+		
+		// If something's on the queue leave it.
+		while(valueExists){
+			wait();
+		}
+
 		this.n = n;
+		valueExists = true;
+		notify();
+
 	}
 
 	public synchronized int get(){
+		
+		while(!valueExists){
+			wait();
+		}
+
+		valueExists = false;
+		notify();
 		return n;
+
 	}
 
 }
