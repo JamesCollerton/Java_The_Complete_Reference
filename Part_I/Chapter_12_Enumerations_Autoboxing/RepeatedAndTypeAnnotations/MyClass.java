3510f0d9d045ad409fe33e47dd03adff4799c8ef
James Collerton <jamescollerton@live.co.uk>
2017-11-20 20:54:50 +0000

    Quick commit before I see why no annotations holding

--- Part_I/Chapter_12_Enumerations_Autoboxing/RepeatedAndTypeAnnotations/MyClass.java
+++ Part_I/Chapter_12_Enumerations_Autoboxing/RepeatedAndTypeAnnotations/MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
 
-	@MyTypeAnnotation(name = "Name 1")
-	@MyTypeAnnotation(name = "Name 2")
-	public int testVariable;
+	@MyTypeAnnotation(name = "Name 1", value = 2)
+	@MyTypeAnnotation(name = "Name 2", value = 3)
+	public String myField = null;
 
 } 
--- Part_I/Chapter_12_Enumerations_Autoboxing/RepeatedAndTypeAnnotations/RepeatedAndTypeAnnotations.java
+++ Part_I/Chapter_12_Enumerations_Autoboxing/RepeatedAndTypeAnnotations/RepeatedAndTypeAnnotations.java
@@ -1,6 +1,34 @@
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
 public class RepeatedAndTypeAnnotations {
 
 	public static void main(String[] args){
+
+		try {
+			Class<?> myClass = new MyClass().getClass();
+			Field myField = myClass.getField("myField");
+
+			Field[] classFields = myClass.getDeclaredFields();
+
+			for(Field field: classFields){
+				System.out.println("woooow");
+				Annotation[] fieldAnnotations = myField.getDeclaredAnnotations();
+				for(Annotation annotation: fieldAnnotations){
+					System.out.println(annotation.toString());
+				}
+				
+			}
+
+			Annotation[] annotations = myField.getDeclaredAnnotations();
+
+			for(Annotation annotation: annotations){
+				System.out.println(annotation.toString());
+			}
+
+		} catch(Exception e){
+			System.out.println("In main exception");
+		}
 
 	}
 
