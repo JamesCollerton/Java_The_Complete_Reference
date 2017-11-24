a5a0dbaacbb6043c4b9467d00d38fc1139c1fba2
James Collerton <jamescollerton@live.co.uk>
2017-11-24 08:01:35 +0000

    Working on bounded example

---
+++ Part_I/Chapter_14_Generics/BoundedWildcardHeirarchy/BoundedWildcardHeirarchyExample.java
@@ -0,0 +1,10 @@
+public class BoundedWildcardHeirarchyExample {
+
+	public static void main(String[] args){
+
+		FourD fourD = new FourD(1, 2, 3, 4);
+		fourD.printAllDimensions();
+
+	}
+
+}
---
+++ Part_I/Chapter_14_Generics/BoundedWildcardHeirarchy/TwoD.java
@@ -0,0 +1,19 @@
+public class TwoD {
+
+	private int x;
+	private int y;
+
+	TwoD(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+}
