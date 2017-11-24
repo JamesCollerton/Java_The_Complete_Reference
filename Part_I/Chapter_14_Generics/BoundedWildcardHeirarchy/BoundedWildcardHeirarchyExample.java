296bda02ef74652ef05d853f6f72a7adef66bd92
James Collerton <jamescollerton@live.co.uk>
2017-11-24 08:23:08 +0000

    Done the bounded wildcard example, not really sure why you would use this and not bounded generics for the method

--- Part_I/Chapter_14_Generics/BoundedWildcardHeirarchy/BoundedWildcardHeirarchyExample.java
+++ Part_I/Chapter_14_Generics/BoundedWildcardHeirarchy/BoundedWildcardHeirarchyExample.java
@@ -2,8 +2,17 @@ public class BoundedWildcardHeirarchyExample {
 
 	public static void main(String[] args){
 
-		FourD fourD = new FourD(1, 2, 3, 4);
-		fourD.printAllDimensions();
+		FourD fourDOne = new FourD(1, 2, 3, 4);
+		FourD fourDTwo = new FourD(5, 6, 7, 8);
+		FourD fourDThree = new FourD(9, 10, 11, 12);
+		FourD fourDFour = new FourD(13, 14, 15, 16);
+
+		fourDOne.printAllDimensions();
+
+		FourD[] fourDArr = new FourD[]{fourDOne, fourDTwo, fourDThree, fourDFour};
+
+		CoordinateArray<FourD> fourDCoordArr = new CoordinateArray<FourD>(fourDArr);
+		fourDCoordArr.printAllXYZ(fourDCoordArr);
 
 	}
 
